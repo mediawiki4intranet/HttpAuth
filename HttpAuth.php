76e3d3453ade69d21e00333c39f629c5781f4c14
Vitaliy Filippov <vitalif@yourcmc.ru>
2010-09-03 14:52:37 +0000

    Bug 68769 - Support for HTTP Basic Auth in MediaWiki

--- HttpAuth.php
+++ HttpAuth.php
@@ -1,6 +1,6 @@
 <?php
 # Copyright (C) 2010 Vitaliy Filippov <vitalif at mail.ru>
-# http://yourcmc.ru/wiki/BasicAuth_(MediaWiki)
+# http://yourcmc.ru/wiki/HttpAuth_(MediaWiki)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

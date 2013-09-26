8b2e2f92014fe3d10b5f723f0622fd9fa817d691
Vitaliy Filippov <vitalif@yourcmc.ru>
2013-09-26 16:54:20 +0400

    Query http auth for ?httpauth=1 even if there is a cookie, add login throttle

--- HttpAuth.php
+++ HttpAuth.php
@@ -1,32 +1,44 @@
 <?php
-# Copyright (C) 2010 Vitaliy Filippov <vitalif at mail.ru>
-# http://yourcmc.ru/wiki/HttpAuth_(MediaWiki)
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-# http://www.gnu.org/copyleft/gpl.html
+/**
+ * Copyright (C) 2010-2013 Vitaliy Filippov <vitalif at mail.ru>
+ * http://wiki.4intra.net/HttpAuth
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+/**
+ * This extension allows to use HTTP Basic authentication in your Wiki.
+ * Just add &httpauth=1 parameter to any URL and it will request HTTP auth.
+ *
+ * WARNING: HTTP Basic authentication is unsecure because transmits user
+ * passwords in cleartext. Use it ONLY either in trusted networks or
+ * over SSL (https://).
+ */
 
 if (!defined('MEDIAWIKI'))
-    die();
+{
+    die('Not an entry point');
+}
 
 $wgExtensionCredits['hook'][] = array(
     'name'          => 'HttpAuth',
     'author'        => 'VitaliyFilippov',
-    'svn-date'      => '$LastChangedDate$',
-    'svn-revision'  => '$LastChangedRevision$',
-    'url'           => 'http://yourcmc.ru/wiki/CharInsertList_(MediaWiki)',
-    'description'   => 'Allows using HTTP Basic authorization in your Wiki',
+    'version'       => '2013-09-26',
+    'url'           => 'http://wiki.4intra.net/HttpAuth',
+    'description'   => 'Allows using HTTP Basic authentication in your Wiki',
     /* TODO: add digest authentication support */
 );
 $wgHooks['UserLoadFromSession'][] = 'efHttpAuthUserLoadFromSession';
@@ -34,40 +46,55 @@ $wgHooks['UserLoadFromSession'][] = 'efHttpAuthUserLoadFromSession';
 function efHttpAuthUserLoadFromSession($user_obj, &$result)
 {
     global $wgSitename, $wgScriptPath, $wgCookiePrefix, $efBasicAuthUnauthHtml, $wgUser;
-    $httpauth = !empty($_GET['httpauth']) ||
-        !empty($_POST['httpauth']);
-    if ($httpauth &&
-        ($n = $_SERVER['PHP_AUTH_USER']) &&
-        ($p = $_SERVER['PHP_AUTH_PW']))
+    $httpauth = !empty($_GET['httpauth']) || !empty($_POST['httpauth']);
+    if ($httpauth)
     {
-        $user = User::newFromName($n);
-        if ($user->getId() && $user->checkPassword($p))
+        $n = isset($_SERVER['PHP_AUTH_USER']) ? $_SERVER['PHP_AUTH_USER'] : NULL;
+        $p = isset($_SERVER['PHP_AUTH_PW']) ? $_SERVER['PHP_AUTH_PW'] : NULL;
+        if ($n && $p)
         {
-            $user_obj->mId = $user->getId();
-            $user_obj->loadFromId();
-            $result = true;
-            wfDebug("Authenticated ".$user_obj->getId()." = User:".$user_obj->getName()." via HTTP\n");
-            return false;
+            $throttleCount = LoginForm::incLoginThrottle($n);
+            if ($throttleCount === true)
+            {
+                wfDebug("HTTP Basic login throttled for $n\n");
+            }
+            else
+            {
+                $user = User::newFromName($n);
+                if ($user->getId() && $user->checkPassword($p))
+                {
+                    $user_obj->mId = $user->getId();
+                    $user_obj->loadFromId();
+                    $result = true;
+                    if ($throttleCount)
+                    {
+                        LoginForm::clearLoginThrottle($n);
+                    }
+                    wfDebug("Authenticated ".$user_obj->getId()." = User:".$user_obj->getName()." via HTTP\n");
+                    return false;
+                }
+            }
         }
-    }
-    if ($httpauth && !$_COOKIE[$wgCookiePrefix.'UserID'])
-    {
         header('HTTP/1.1 401 Unauthorized');
         header('WWW-Authenticate: Basic realm="'.$wgSitename.'"');
         if ($efBasicAuthUnauthHtml !== NULL)
+        {
             print $efBasicAuthUnauthHtml;
+        }
         else
         {
 ?><html>
 <head><title>401 Unauthorized</title></head>
 <body>
 <h1>Authentication Required</h1>
-<p>This server could not verify that you are authorized to access the document requested. Either you supplied the wrong credentials (e.g., bad password), or your browser doesn't understand how to supply the credentials required.</p>
+<p>This server could not verify that you are authorized to access the document requested.
+Either you supplied the wrong credentials (e.g., bad password), or your browser
+doesn't understand how to supply the credentials required.</p>
 <hr>
 <address>MediaWiki <?=$wgVersion?> at <?=$wgServer.$wgScriptPath?></address>
 </body>
 </html><?php
-            exit;
+                exit;
         }
     }
     return true;
